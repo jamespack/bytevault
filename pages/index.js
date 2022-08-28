a1a58fe3047358abdce5879c85ad482a656932c9
James Pack <jamespack@users.noreply.github.com>
2022-08-28 18:57:15 -0400

    Coming soon

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,7 @@ import styles from '../styles/ComingSoon.module.css';
 
 const ComingSoon = () =>{
     return(
-        <div className={styles.container}>
-            <Head>
-                <title>Coming Soon!</title>
-                <meta name="description" content="ByteVault coming soon!" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-        </div>
+        <></>
     )
 }
 
